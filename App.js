3cf8d8e6667d49076981021dbba87f8efb7b47b9
YoganandaNY <136065256+YoganandaNY@users.noreply.github.com>
2023-08-25 12:14:39 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
-// import Body1 from "./components/Body1";
 import Body from "./components/Body";
-//import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
